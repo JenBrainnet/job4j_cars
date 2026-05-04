cd6f7023962764473e942a5cbd27e96873bd7a00
JenBrainnet <benebrak01@gmail.com>
2026-05-04 21:08:55 +0300

    6. Lambdas and the Command Pattern [#49295]

--- src/main/java/ru/job4j/cars/repository/UserRepository.java
+++ src/main/java/ru/job4j/cars/repository/UserRepository.java
@@ -1,17 +1,16 @@
 package ru.job4j.cars.repository;
 
 import lombok.AllArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import ru.job4j.cars.model.User;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @AllArgsConstructor
 public class UserRepository {
 
-    private final SessionFactory sf;
+    private final CrudRepository crudRepository;
 
     /**
      * Save a user to the database.
@@ -19,15 +18,7 @@ public class UserRepository {
      * @return user with id.
      */
     public User create(User user) {
-        try (Session session = sf.openSession()) {
-            var tx = session.beginTransaction();
-            try {
-                session.save(user);
-                tx.commit();
-            } catch (Exception e) {
-                tx.rollback();
-            }
-        }
+        crudRepository.run(session -> session.persist(user));
         return user;
     }
 
@@ -36,15 +27,7 @@ public class UserRepository {
      * @param user user.
      */
     public void update(User user) {
-        try (Session session = sf.openSession()) {
-            var tx = session.beginTransaction();
-            try {
-                session.update(user);
-                tx.commit();
-            } catch (Exception e) {
-                tx.rollback();
-            }
-        }
+        crudRepository.run(session -> session.merge(user));
     }
 
     /**
@@ -52,17 +35,10 @@ public class UserRepository {
      * @param userId ID
      */
     public void delete(Integer userId) {
-        try (Session session = sf.openSession()) {
-            var tx = session.beginTransaction();
-            try {
-                session.createQuery("DELETE User WHERE id = :id")
-                        .setParameter("id", userId)
-                        .executeUpdate();
-                tx.commit();
-            } catch (Exception e) {
-                tx.rollback();
-            }
-        }
+        crudRepository.run(
+                "DELETE User WHERE id = :id",
+                Map.of("id", userId)
+        );
     }
 
     /**
@@ -70,10 +46,7 @@ public class UserRepository {
      * @return list of users.
      */
     public List<User> findAllOrderById() {
-        try (Session session = sf.openSession()) {
-            return session.createQuery("FROM User ORDER BY id ASC", User.class)
-                    .list();
-        }
+        return crudRepository.query("FROM User ORDER BY id ASC", User.class);
     }
 
     /**
@@ -82,11 +55,10 @@ public class UserRepository {
      * @return user.
      */
     public Optional<User> findById(Integer userId) {
-        try (Session session = sf.openSession()) {
-            return session.createQuery("FROM User WHERE id = :id", User.class)
-                    .setParameter("id", userId)
-                    .uniqueResultOptional();
-        }
+        return crudRepository.optional(
+                "FROM User WHERE id = :id", User.class,
+                Map.of("id", userId)
+        );
     }
 
     /**
@@ -95,12 +67,10 @@ public class UserRepository {
      * @return list of users.
      */
     public List<User> findByLikeLogin(String key) {
-        try (Session session = sf.openSession()) {
-            return session.createQuery(
-                    "FROM User WHERE login LIKE :key ORDER BY id ASC", User.class)
-                    .setParameter("key", "%" + key + "%")
-                    .list();
-        }
+        return crudRepository.query(
+                "FROM User WHERE login LIKE :key ORDER BY id ASC", User.class,
+                Map.of("key", "%" + key + "%")
+        );
     }
 
     /**
@@ -109,11 +79,10 @@ public class UserRepository {
      * @return Optional or user.
      */
     public Optional<User> findByLogin(String login) {
-        try (Session session = sf.openSession()) {
-            return session.createQuery("FROM User WHERE login = :login", User.class)
-                    .setParameter("login", login)
-                    .uniqueResultOptional();
-        }
+        return crudRepository.optional(
+                "FROM User WHERE login = :login", User.class,
+                Map.of("login", login)
+        );
     }
 
 }
